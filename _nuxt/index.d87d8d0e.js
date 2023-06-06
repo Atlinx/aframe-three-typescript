0d6bf1b045865ebf993dff584629a9568a9d6621
Atlinx <atlinuxtm@gmail.com>
2023-06-06 18:47:02 +0000

    Deploying to gh-pages from @ Atlinx/nuxt-aframe@a4fccaa6edad7c665c2e59fd3ed33be894c4b601 🚀

---
+++ _nuxt/index.d87d8d0e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./SectionBody.vue.861af37d.js";import{l as o,c,w as s,o as n,b as a}from"./entry.41a4e24a.js";const _={},r=a("h1",{class:"text-section-title mt-16 md:mb-8"},"Nuxt AFrame",-1);function i(m,d){const t=e;return n(),c(t,null,{default:s(()=>[r]),_:1})}const x=o(_,[["render",i]]);export{x as default};
